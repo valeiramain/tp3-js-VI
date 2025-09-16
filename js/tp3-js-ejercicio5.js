6d2f12b2d688865986b5320aaaaf534d3519d6f6
Valentina Iramain <valentinairamain2020@gmail.com>
2025-09-16 09:59:05 -0300

    Se realiza ejercicio 5

---
+++ js/tp3-js-ejercicio5.js
@@ -0,0 +1,16 @@
+//5- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
+
+function mayusculaOMinuscula(cadena) {
+    if (cadena === cadena.toUpperCase()) {
+        document.writeln(`<p>La cadena: "${cadena}" está formada solo por MAYÚSCULAS.</p>`);
+    } else if (cadena === cadena.toLowerCase()) {
+        document.writeln(`<p>La cadena: "${cadena}" está formada solo por minúsculas.</p>`);
+    } else {
+        document.writeln(`<p>La cadena: "${cadena}" contiene una mezcla de mayúsculas y minúsculas.</p>`);
+    }
+}
+
+
+const texto = prompt('Ingrese un cadena de texto:')
+
+mayusculaOMinuscula(texto)

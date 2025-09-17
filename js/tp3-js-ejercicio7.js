4b21f0d21be1e674e3d08482f7cb8a2de80e9cd0
Valentina Iramain <valentinairamain2020@gmail.com>
2025-09-17 11:41:28 -0300

    Se corrige para usar un array

--- js/tp3-js-ejercicio7.js
+++ js/tp3-js-ejercicio7.js
@@ -1,16 +1,50 @@
-// Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
+// Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, 
+// la creación de la tabla debe ser realizada con una función y 
+// mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
 
-function tablaMultiplicar(numero){
-    document.writeln(`<h4 class="my-3">Tabla de Multiplicar por ${numero}</h4>`)
-    for(let i=0; i<=12; i++){
-        document.writeln(`<p>${numero} X ${i} = ${numero*i}</p>`)
+
+const tabla = [0,0,0,0,0,0,0,0,0,0,0]
+
+function tablaMultiplicar(nro){
+    for(let i=1; i < 11 ;i++){
+        tabla[i] = i * nro;
     }
+    return
 }
 
-const numeroTabla = parseInt(prompt('Ingrese un número para la tabla de multiplicar: '))
+const mostrarTabla = (titulo,nro) => {
+    document.writeln(`<h3>${titulo}</h3>`)
 
-if(!isNaN(numeroTabla)){
-    tablaMultiplicar(numeroTabla)
+    document.writeln(
+        `<table class="table table-bordered table-striped text-center p-2">
+            <thead>
+                <tr>
+                    <th>Número</th>
+                    <th>Resultado</th>
+                </tr>
+            </thead>
+            <tbody>`);
+
+    document.writeln(`<ul>`)
+    for (i = 1; i < 11; i++) {
+        document.writeln(`<tr>`)
+        document.writeln(`<td>${nro} x ${i}</td>`)
+        document.writeln(`<td>${tabla[i]}</td>`)
+        document.writeln(`</tr>`)
+    }
+    document.writeln(`
+        </tbody>
+    </table>`)
+
+    return
+}
+
+const numero = parseFloat(prompt('Ingrese un número:'))
+
+if ((!isNaN(numero))&&(numero!==undefined)){
+    tablaMultiplicar(numero)
+
+    mostrarTabla(`Tabla de Multiplicar del ${numero}:`,numero)
 }else{
-    document.writeln(`<p>Debe ingresar un número</p>`)
+    document.writeln(`Operación Inválida`)
 }
